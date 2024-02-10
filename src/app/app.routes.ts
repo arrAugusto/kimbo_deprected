bc13fb8013845f2061c38bd51cf3e5118a626504
Augusto Gomez <agr120@hotmail.com>
2024-02-09 22:40:55 -0600

    Stared navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
-export const routes: Routes = [{
-    path: '', component: LoginComponent
-}];
+export const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'dashboard', component: NavigationBarComponent},
+    {path: 'sidebar', component: SideBarComponent}
+];
