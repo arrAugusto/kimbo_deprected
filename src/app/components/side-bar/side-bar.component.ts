39d982e4a9b19e9271665203ddb0da07aa9a1c69
Augusto Gomez <agr120@hotmail.com>
2024-02-10 22:43:59 -0600

    SIDENAV Y NAV BAR

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common'; // Importa la función isPlatformBrowser
+import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
+
 
 @Component({
   selector: 'app-side-bar',
-  standalone: true,
-  imports: [],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.css'
+  styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
 
+  
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) { // Verifica si estamos en un navegador
+      // Importa y ejecuta el código de Materialize solo si estamos en un navegador
+      import('materialize-css').then((M) => {
+        // Inicializa el sidenav
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems);
+      });
+    }
+  }
 }
